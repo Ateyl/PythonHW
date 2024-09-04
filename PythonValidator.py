6396d6dbe795069c263f94afe94fe6b347ab7674
Ateyl <153456054+Ateyl@users.noreply.github.com>
2024-09-04 16:52:03 +0300

    Python_ID_Validator

--- PythonValidator.py
+++ PythonValidator.py
@@ -1,8 +1,11 @@
+from math import remainder
+
+
 def ask_for_id():
     global isikukood
     while True:
         idcode = input('Please enter your national id or "stop" to exit: ')
-        if idcode == 'stop':
+        if idcode.lower() == 'stop':
             quit()
         if idcode.isdecimal():
             if len(idcode) != 11:
@@ -52,7 +55,77 @@ def get_date_of_birth():
     else:
         print('something wrong with your id')
 
+def get_birth_of_region():
+    birth_number = int(isikukood[7:10])
+    if birth_number in range (1,11):
+        print('You were born in "Kuressaare haigla"')
+    elif birth_number in range (11,20):
+        print('You were born in "Tartu Ülikooli Naistekliinik"')
+    elif birth_number in range (21,151):
+        print('You were born in "Ida-Tallinna keskhaigla, Pelgulinna sünnitusmaja (Tallinn)"')
+    elif birth_number in range (151,161):
+        print('You were born in "Keila haigla"')
+    elif birth_number in range (161,221):
+        print('You were born in "Rapla haigla, Loksa haigla, Hiiumaa haigla (Kärdla)"')
+    elif birth_number in range(221,271):
+        print('You were born in "Ida-Viru keskhaigla (Kohtla-Järve, endine Jõhvi)"')
+    elif birth_number in range (271,371):
+        print('You were born in "Maarjamõisa kliinikum (Tartu), Jõgeva haigla"')
+    elif birth_number in range (371,421):
+        print('You were born in "Narva haigla"')
+    elif birth_number in range (421,471):
+        print('You were born in "Pärnu haigla"')
+    elif birth_number in range (471,491):
+        print('You were born in "Haapsalu haigla"')
+    elif birth_number in range (491,521):
+        print('You were born in "Järvamaa haigla (Paide)"')
+    elif birth_number in range (521,571):
+        print('You were born in "Rakvere haigla, Tapa haigla"')
+    elif birth_number in range (571,601):
+        print('You were born in "Valga haigla"')
+    elif birth_number in range (601,651):
+        print('You were born in "Viljandi haigla"')
+    elif birth_number in range (651,701):
+        print('You were born in "Lõuna-Eesti haigla (Võru), Põlva haigla"')
+    else:
+        print('Unknown birth region')
 
+
+def get_validation():
+    val_kood = [int(val) for val in isikukood]
+    # print(val_kood)
+    kaal1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
+    kaal2 = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
+    sum1 = sum(val_kood[i] * kaal1[i] for i in range(10))
+    left1 = sum1 % 11
+
+    if left1 < 10:
+        control_num = left1
+    else:
+        sum2 = sum(val_kood[i] * kaal2[i] for i in range(10))
+        left2 = sum2 % 11
+        if left2 < 10:
+            control_num = left2
+        else:
+            control_num = 0
+    if control_num == int(isikukood[10]):
+        print('Your id is valid')
+    else:
+        print('Your id is invalid')
+
+
+
+#
+# val_kood[0] * weights_1[0] +
+# val_kood[1] * weights_1[1] +
+# val_kood[2] * weights_1[2] +
+# val_kood[3] * weights_1[3] +
+# val_kood[4] * weights_1[4] +
+# val_kood[5] * weights_1[5] +
+# val_kood[6] * weights_1[6] +
+# val_kood[7] * weights_1[7] +
+# val_kood[8] * weights_1[8] +
+# val_kood[9] * weights_1[9] +
 
 
 # idcode = '38803160272'
@@ -74,9 +147,9 @@ def menu():
         elif user_choice == '2':
             get_date_of_birth()
         elif user_choice == '3':
-            print(3,isikukood)
+            get_birth_of_region()
         elif user_choice == '4':
-            print(4,isikukood)
+            get_validation()
         elif user_choice == '5':
             ask_for_id()
         elif user_choice == '0' or user_choice == 'stop':
